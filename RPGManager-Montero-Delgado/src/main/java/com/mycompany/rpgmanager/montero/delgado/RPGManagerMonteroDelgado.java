5683e8e574d8ecc7d09edc92ce9bc59ea114abf2
Juan David Delgado Maldonado <juddelgadom@udistrital.edu.co>
2026-05-02 18:16:01 -0500

    [nivelacion-n1] setup inicial - estructura del proyecto

---
+++ RPGManager-Montero-Delgado/src/main/java/com/mycompany/rpgmanager/montero/delgado/RPGManagerMonteroDelgado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpgmanager.montero.delgado;
+
+/**
+ *
+ * @author DeathStar
+ */
+public class RPGManagerMonteroDelgado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
